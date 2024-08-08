fa18759f20f0d8590c4bd37d916de3e4868b30b2
jose <impacevanend@gmail.com>
2024-08-08 14:41:03 -0500

    se agrega manipulación básica de datos

--- manipulacion_basica.py
+++ manipulacion_basica.py
@@ -5,3 +5,20 @@ Created on Thu Aug  8 13:57:30 2024
 @author: luis.bravo
 """
 
+import pandas as pd
+
+# Cargar el archvio CSV de ventas
+df_ventas = pd.read_csv('ventas.csv')
+
+# Mostrar las primeras filas del DataFrame
+
+print(df_ventas.head())
+
+#INSPECCIÓN DE DATOS
+
+#Inspeccionar el DataFrame
+print("Información del DataFrame de ventas:")
+print(df_ventas.info())
+
+print("\nDescripción estadística del DataFrame de ventas")
+print(df_ventas.describe())

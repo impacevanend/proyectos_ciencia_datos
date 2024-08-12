33f69b3c1ce38a9718583140109f28b2dee55dea
jose <impacevanend@gmail.com>
2024-08-12 17:55:02 -0500

    ingresando información adicional a los datos

--- Análisis_Transformacion_Datos_2/Analisis_Transformación_Datos.py
+++ Análisis_Transformacion_Datos_2/Analisis_Transformación_Datos.py
@@ -21,7 +21,7 @@ grupo_producto = df_ventas.groupby('Producto')['Monto'].sum()
 # Mostrar los resultados
 print(grupo_producto)
 
-# Aplicación de funciones: 'apply' y 'map'
+#2. Aplicación de funciones: 'apply' y 'map'
 
 # Definiendo una función de limpieza
 # Función para limpiar fechas
@@ -37,4 +37,41 @@ df_ventas['Fecha'] = df_ventas['Fecha'].apply(limpiar_fecha)
 
 # Mostrar las primeras filas para verificar
 print(df_ventas.head())
+
+#3. Combinación de DataFrames: merge y concat
+
+
+# CREAR DATAFRAME SIMULADO DE PRODUCTOS
+data_categorias = {'Producto':['A','B','C','D'],
+                   'Categoría':['Electrónica', 'Ropa', 'Hogar', 'Deportes']}
+
+df_categorias = pd.DataFrame(data_categorias)
+
+# Mostrar el dataFrame de categorías
+
+print(df_categorias)
+
+# paso 2 combinar los dataFrame
+# Unir los DataFrames en base a la columna 'Producto'
+
+df_combinado = pd.merge(df_ventas, df_categorias, on='Producto', how='left')
+
+# Mostrar las primeras filas del DataFrame combinado
+print(df_combinado.head())
+
+# 4. Calcular Estadísticas Básicas: Media, Mediana, Moda
+
+# Calcular la media del monto
+media_monto = df_ventas['Monto'].mean()
+print(f"Media del monto: {media_monto}")
+
+# Calcular la mediana del monto
+mediana_monto = df_ventas['Monto'].median()
+print(f"Mediana del monto: {media_monto}")
+
+# Calcula la moda del monto
+moda_monto = df_ventas['Monto'].mode()
+print(f"Moda del monto: {moda_monto[0]}") 
+
+
 
